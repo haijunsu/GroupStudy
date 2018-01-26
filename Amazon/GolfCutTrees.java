47814535200546fb1f407f248b248d42d8ce4468
Haijun Su <navysu@gmail.com>
2018-01-26 16:23:42 -0500

    Golf and Unique Chars

---
+++ Amazon/GolfCutTrees.java
@@ -0,0 +1,88 @@
+/*
+ * Leetcode: https://leetcode.com/problems/cut-off-trees-for-golf-event/description/
+ * It likes Maze problem but each position has four directories.
+ */
+
+import java.util.*;
+class GolfCutTrees {
+    // directions
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    // dimension
+    static int X = 0;
+    static int Y = 0;
+
+    public static int cutOffTree(List<List<Integer>> forest) {
+        X = forest.size();
+        Y = forest.get(0).size();
+        // sort tree
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>((int[]a, int[] b) -> Integer.compare(a[2], b[2]));
+
+        for (int x = 0; x < X; ++x) {
+            for (int y = 0; y < Y; ++y) {
+                int val = forest.get(x).get(y);
+                if (val > 1) {
+                    queue.offer(new int[]{x, y, val});
+                }
+            }
+        }
+        int steps = 0, sx = 0, sy = 0;
+        while(!queue.isEmpty()) {
+            int[] tree = queue.poll();
+            int dist = dist(forest, sx, sy, tree[0], tree[1]);
+            if (dist < 0) return -1;
+            steps += dist;
+            sx = tree[0];
+            sy = tree[1];
+        }
+        return steps;
+    }
+
+    static int dist(List<List<Integer>> forest, int sx, int sy, int tx, int ty) {
+        Queue<int[]> processQueue = new LinkedList<int[]>();
+        boolean[][] visited = new boolean[X][Y];
+        processQueue.offer(new int[]{sx, sy, 0});
+        visited[sx][sy] = true;
+        while(!processQueue.isEmpty()) {
+            int[] curr = processQueue.poll();
+            if (curr[0] == tx && curr[1] == ty) {
+                return curr[2];
+            }
+            for (int i = 0; i < 4; ++i) {
+                int x = curr[0] + dx[i];
+                int y = curr[1] + dy[i];
+                if (x >= 0 && x < X && y >= 0 && y < Y && !visited[x][y] && forest.get(x).get(y) > 0) {
+                    visited[x][y] = true;
+                    processQueue.add(new int[]{x, y, curr[2] + 1});
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[][] testData = new int[][]{{1,2,3},{0,0,4},{7,6,5}};
+        test(testData, 6);
+        testData = new int[][]{{1,2,3},{0,0,0},{7,6,5}};
+        test(testData, -1);
+
+    }
+
+    private static List<List<Integer>> buildList(int[][] data) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < data.length; ++i) {
+            List<Integer> items = new ArrayList<Integer>();
+            for (int j = 0; j < data[i].length; ++j) {
+                items.add(data[i][j]);
+            }
+            result.add(items);
+        }
+        return result;
+    }
+
+    private static void test(int[][] data, int expected) {
+        int result = cutOffTree(buildList(data));
+        System.out.println("Yours: " + result + ", excepted: " + expected);
+        System.out.println(result == expected?"Accept":"Wrong");
+    }
+}
---
+++ haijun/UniqueChars.java
@@ -0,0 +1,45 @@
+/**
+ * Impletement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+ * Source: Cracking the code interview book.
+ */
+
+import java.util.*;
+class UniqueChars {
+
+    public static boolean isUniqueChars (String str) {
+        int checker = 0;
+        for (int i = 0; i < str.length(); ++i) {
+            int val  = str.charAt(i) - 'a';
+            if ((checker & (1 << val)) > 0) return false;
+            checker |= (1 << val);
+        }
+        return true;
+    }
+
+    public static boolean isUniqueChars2 (String str) {
+        // practice Bitset. Not for this question
+        BitSet visited = new BitSet();
+        for (int i = 0; i < str.length(); ++i) {
+            int val = str.charAt(i) - 'a';
+            if (visited.get(val)) return false;
+            visited.set(val);
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        String str = "abcdefgfedcba";
+        System.out.println("False");
+        System.out.println(isUniqueChars(str));
+        System.out.println(isUniqueChars2(str));
+        System.out.println("===========");
+        str = "aaaa";
+        System.out.println("False");
+        System.out.println(isUniqueChars(str));
+        System.out.println(isUniqueChars2(str));
+        System.out.println("===========");
+        str = "abcde";
+        System.out.println("True");
+        System.out.println(isUniqueChars(str));
+        System.out.println(isUniqueChars2(str));
+    }
+}
