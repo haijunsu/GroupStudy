29d3b28eda3c167230349edad2c6f9b5481f3773
Haijun Su <navysu@gmail.com>
2021-12-16 09:53:34 -0500

    add BasicAuthWithSpring

---
+++ haijun/java/BasicAuthWithSpring/src/main/java/com/navysu/basic/auth/BasicAuthWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.navysu.basic.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthWithSpringApplication.class, args);
+	}
+
+}
