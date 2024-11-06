d402d3aea52df8096836b735b3e852eb638aa518
Haijun Su <navysu@gmail.com>
2024-11-06 08:58:09 -0500

    add desc

--- haijun/java/JavaBasic/src/main/java/com/navysu/java/basic/algorithm/GenerateParenthesis.java
+++ haijun/java/JavaBasic/src/main/java/com/navysu/java/basic/algorithm/GenerateParenthesis.java
@@ -1,5 +1,13 @@
 package com.navysu.java.basic.algorithm;
 
+/**
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ * 示例：
+ *
+ * 输入：n = 3 输出：[ "((()))", 000111 "(()())", 001011 "(())()", 001101 "()(())",
+ * 010011 "()()()" 010101 ]
+ */
 public class GenerateParenthesis {
 
     public static void main(String[] args) {
