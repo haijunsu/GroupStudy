95921a5bd20060744378966d4ad66822f6379328
Haijun Su <navysu@gmail.com>
2024-11-16 13:11:51 -0500

    setRef

--- haijun/react/react-basic/src/App.js
+++ haijun/react/react-basic/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
 
 
@@ -7,6 +7,16 @@ const cities = ["Tokyo", "Tahoe", "Beijing", "New York", "San Francisco", "Seoul
 function App() {
   const [emotion, setEmotion] = useState("Hello");
   const [checked, setChecked] = useReducer((checked) => !checked, false);
+  const textTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+    const title = textTitle.current.value;
+    const color = hexColor.current.value;
+    console.log(title, color);
+  }
 
   useEffect(() => {
     console.log(`Current emotion is ${emotion}`);
@@ -17,13 +27,22 @@ function App() {
       <h1>
         Hello current emotion is {emotion}
       </h1>
-      <button onClick={() => setEmotion("Happy")}>Happy.</button>
-      <button onClick={() => setEmotion("Sad")}>Sad</button>
-      <button onClick={() => setEmotion("Angry")}>Angry</button>
-      <div>
-        <input type="checkbox" checked={checked} onChange={setChecked} />
-        {checked ? "checked" : "not checked"}
-      </div>
+      <form onSubmit={submit}>
+        <button onClick={() => setEmotion("Happy")}>Happy.</button>
+        <button onClick={() => setEmotion("Sad")}>Sad</button>
+        <button onClick={() => setEmotion("Angry")}>Angry</button>
+        <div>
+          <input type="checkbox" checked={checked} onChange={setChecked} />
+          {checked ? "checked" : "not checked"}
+        </div>
+        <div>
+          <input ref={textTitle} type="text" placeholder="color title ...">
+          </input>
+          <input ref={hexColor} type="color" />
+          <button>ADD</button>
+        </div>
+
+      </form>
     </div>
   );
 }
