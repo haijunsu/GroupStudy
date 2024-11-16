3309a98be5e52c55eb7475ec3690ebc7674a0a35
Haijun Su <navysu@gmail.com>
2024-11-16 12:40:36 -0500

    useEffect and useReducer

--- haijun/react/react-basic/src/App.js
+++ haijun/react/react-basic/src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useReducer, useState } from 'react';
 import './App.css';
 
+
+const cities = ["Tokyo", "Tahoe", "Beijing", "New York", "San Francisco", "Seoul"];
+
 function App() {
+  const [emotion, setEmotion] = useState("Hello");
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
+
+  useEffect(() => {
+    console.log(`Current emotion is ${emotion}`);
+  }, [emotion])
+
   return (
     <div className="App">
-      Hello from Navy.
+      <h1>
+        Hello current emotion is {emotion}
+      </h1>
+      <button onClick={() => setEmotion("Happy")}>Happy.</button>
+      <button onClick={() => setEmotion("Sad")}>Sad</button>
+      <button onClick={() => setEmotion("Angry")}>Angry</button>
+      <div>
+        <input type="checkbox" checked={checked} onChange={setChecked} />
+        {checked ? "checked" : "not checked"}
+      </div>
     </div>
   );
 }
