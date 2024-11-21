890cbbadee0e786b39c6a5c05353834a5b8ebdfb
Haijun Su <navysu@gmail.com>
2024-11-21 16:14:00 -0500

    add test cases

--- haijun/java/JavaBasic/src/main/java/com/navysu/java/basic/algorithm/matrix/SearchMatrix.java
+++ haijun/java/JavaBasic/src/main/java/com/navysu/java/basic/algorithm/matrix/SearchMatrix.java
@@ -38,7 +38,21 @@ package com.navysu.java.basic.algorithm.matrix;
  */
 public class SearchMatrix {
     public static void main(String[] args) {
+        int[][] matrix = { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 } };
+        System.out.println(searchMatrix(matrix, 5));
+        System.out.println(searchMatrix(matrix, 20));
 
+        matrix = new int[][] { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 } };
+        System.out.println(searchMatrix(matrix, 20));
+        matrix = new int[][] { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 } };
+        System.out.println(searchMatrix(matrix, 13));
+
+        matrix = new int[][] { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 } };
+        System.out.println(searchMatrix(matrix, 1));
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
