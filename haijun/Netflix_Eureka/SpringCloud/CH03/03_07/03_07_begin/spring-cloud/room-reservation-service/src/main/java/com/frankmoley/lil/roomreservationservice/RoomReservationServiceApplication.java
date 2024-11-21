3feb690f1ea73ea56ef4c3c9937bdcfd923f1193
Haijun Su <navysu@gmail.com>
2024-11-21 08:30:56 -0500

    add netflix eureka

---
+++ haijun/Netflix_Eureka/SpringCloud/CH03/03_07/03_07_begin/spring-cloud/room-reservation-service/src/main/java/com/frankmoley/lil/roomreservationservice/RoomReservationServiceApplication.java
@@ -0,0 +1,24 @@
+package com.frankmoley.lil.roomreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RoomReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomReservationServiceApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
